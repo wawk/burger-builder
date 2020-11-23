00974d1dfc29fe6d2bb97f69f2d06d03348df454
wawk <white_knight@prodigy.net>
2020-11-23 08:59:50 -0800

    added routing and checkout page'

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,37 +83,38 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert("You continue!");
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max Swarzmuller',
-                address: {
-                    street: 'TestStreet',
-                    zipCode: '99567',
-                    country: 'Germany',
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Max Swarzmuller',
+        //         address: {
+        //             street: 'TestStreet',
+        //             zipCode: '99567',
+        //             country: 'Germany',
 
 
 
-                },
-                email: 'test@someemail.com'
+        //         },
+        //         email: 'test@someemail.com'
 
 
-            },
-            deliverMethod: 'fastest'
+        //     },
+        //     deliverMethod: 'fastest'
 
-        };
+        // };
 
-        axiosInstance.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false,  purchasing: false});
+        // axiosInstance.post('/orders.json', order)
+        // .then(response => {
+        //     this.setState({loading: false,  purchasing: false});
 
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false});
+        // })
+        // .catch(error => {
+        //     this.setState({loading: false, purchasing: false});
 
-        });
+        // });
+        this.props.history.push('/checkout');
 
     };
 
